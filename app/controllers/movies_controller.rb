19dc939a1524156aa97f4233dfeafc1f2b8962df
Ernie Salazar <ehsalazar75@gmail.com>
2015-03-20 01:11:53 -0700

    Add CRUD actions for movie.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,48 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movies.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Successfully created new movie!"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie was successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :movie_length, :director, :rating)
+  end
+
 end
