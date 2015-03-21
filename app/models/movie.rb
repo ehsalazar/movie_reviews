55d45de7249b4365881c2cc1eee157528ef9b054
Ernie Salazar <ehsalazar75@gmail.com>
2015-03-20 17:47:15 -0700

    Clean up styling and add structure to forms.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
   searchkick
   belongs_to :user
   has_many :reviews
-  has_attached_file :image, styles: { medium: "400x600#" }
+  has_attached_file :image, styles: { medium: "350x550#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
