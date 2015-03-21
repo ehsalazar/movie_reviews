852e4e9712c53eabb5c8d7e98e55f274fa850805
Ernie Salazar <ehsalazar75@gmail.com>
2015-03-20 17:02:46 -0700

    Add ability to search through movies.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :movies do
-    # collection do
-    #   get 'search'
-    # end
+    collection do
+      get 'search'
+    end
     resources :reviews, except: [:show, :index]
   end
 
